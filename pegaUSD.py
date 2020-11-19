eae4a79e0757831fe19d1f8cdb0305ac7ccaf529
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-19 19:13:28 -0300

    Update pegaUSD.py

--- pegaUSD.py
+++ pegaUSD.py
@@ -5,8 +5,10 @@ URL = 'http://www.b3.com.br/pt_br/busca/?query=bbas3'
 headers  = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
 page = requests.get(URL, headers = headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-# cotacaoBBAS3 = soup.find()
+# print(soup.prettify())
+cotacaoBBAS3 = soup.find(title="Last price")
+print(cotacaoBBAS3)
+
 
 
 
