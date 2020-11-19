31b3ed20bb918031f3b5a400acb09d99acfd1814
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-19 20:54:23 -0300

    Update pegaUSD.py

--- pegaUSD.py
+++ pegaUSD.py
@@ -1,27 +1,16 @@
 import requests
-from bs4 import BeautifulSoup
-URL = 'http://www.b3.com.br/pt_br/busca/?query=bbas3'
-# my user agent
-headers  = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
-page = requests.get(URL, headers = headers)
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
-cotacaoBBAS3 = soup.find(title="Last price")
-print(cotacaoBBAS3)
-
-
-
-
-
-
-
-
-
-"""
 import pandas as pd
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get(url)
-"""
+# from selenium.webdriver.firefox.options import Options
+import json
+
+# pegar conteudo HTML pela url
+url = 'http://www.b3.com.br/pt_br/busca/?query=bbas3'
+driver.get(url)
+
+cotacao = driver.find_element_by_xpath("//div[@class='tv-widget-chart__price'])
+html_content = cotacao.get_attribute('outerHTML')
+
+soup = BeautifulSoup(html_content, 'html_parser')
+table = soup.find_next
