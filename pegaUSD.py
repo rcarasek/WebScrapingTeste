a130275af9d41fd8b1dd158fbcc14652d69e9c39
Ricardo Carasek <50562794+rcarasek@users.noreply.github.com>
2020-11-19 15:46:51 -0300

    Update pegaUSD.py

--- pegaUSD.py
+++ pegaUSD.py
@@ -1,18 +1,19 @@
-
-import pandas as pd
-from bs4 import BeautifulSoup
-
-from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.chrome.options import Options
-import json
-
-
 import requests
+from bs4 import BeautifulSoup
+URL = 'https://www.bcb.gov.br/'
+# my user agent
+headers  = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+
+
+page = requests.get(URL, headers = headers)
+print(page)
+
+
+"""
+import pandas as pd
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-url = 'https://www.bcb.gov.br/'
 browser = webdriver.Chrome()
 browser.get(url)
-
-# my user agent
-# https://www.google.com/search?q=my+user+agent&oq=my+user+&aqs=chrome.1.69i57j0l2j0i22i30j0i10i22i30j0i22i30l2j69i60.3479j0j1&sourceid=chrome&ie=UTF-8
-# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36
+"""
